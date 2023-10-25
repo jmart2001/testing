0aafa7f023a6d094dc863ee1cad6a513eead6534
Justin Matthew Martin <justinmmartin01@gmail.com>
2023-10-25 10:39:57 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Capstone</h1>
+      <h2>Here is my codebase!</h2>
+    </div>
+  );
+}
