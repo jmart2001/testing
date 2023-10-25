d810d1c38c6072ee4833e24d2434b4252aedd062
Valerie Duffey <vduffey@email.sc.edu>
2023-10-25 11:06:37 -0400

    added button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./styles.css";
 
+function Capstone() {
+  return (
+    <a href="https://capstone.cse.sc.edu/milestone/source-control/" target="_blank">
+    <button style = {{color: 'white', fontSize: '20px', background: 'gray'}}>
+      Capstone
+    </button>
+    </a>
+  );
+}
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello Capstone</h1>
       <h2>Here is my codebase!</h2>
       <h2>Here is another commit</h2>
+      <h2>Here is a link to the website</h2>
+      <h2 style={{textAlign: 'center'}}><Capstone/></h2>
     </div>
   );
 }
