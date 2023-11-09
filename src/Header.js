46cf6f4d561618e40e7cd9cd48e3757a31caf93d
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-09 09:37:19 -0500

    NoBullets

--- src/Header.js
+++ src/Header.js
@@ -14,26 +14,15 @@ function Header() {
         <div className="logo">WhatToCook</div>
         <nav className="nav">
           <ul className="nav-list">
-            <li className="nav-item">
               <a href="/">Home</a>
+            <li className="nav-item">
+              <a href="/RepGen">Recipe Generator</a>
             </li>
             <li className="nav-item">
-              <a href="/about">About</a>
-            </li>
-            <li className="nav-item dropdown">
-              <button onClick={toggleDropdown} className="dropdown-btn">
-                Services
-              </button>
-              {isDropdownOpen && (
-                <div className="dropdown-content">
-                  <a href="/service1">Service 1</a>
-                  <a href="/service2">Service 2</a>
-                  <a href="/service3">Service 3</a>
-                </div>
-              )}
+              <a href="/TriCom">Price Comparator</a>
             </li>
             <li className="nav-item">
-              <a href="/contact">Contact</a>
+              <a href="/Profile">Profile</a>
             </li>
           </ul>
         </nav>
