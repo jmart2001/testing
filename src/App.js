d4a9653755853fb63b566020b517fae24c3a4ac4
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-09 12:41:59 -0500

    More

--- src/App.js
+++ src/App.js
@@ -15,10 +15,8 @@ function Capstone() {
 
 export default function App() {
   return (
-    <div className="App">
+    <div style={{background: 'lemonchiffon'}} className="App">
       <h1 style={{background: 'lemonchiffon'}}><Header/></h1>
-      <h1>Hello Capstone</h1>
-      <h2 style={{textAlign: 'center'}}><Capstone/></h2>
     </div>
   );
 }
