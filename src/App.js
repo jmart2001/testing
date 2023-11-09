8dbaaf1cd2e5eb9954ad6316b0184451988cc02b
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-09 09:22:27 -0500

    NewHeaderStyle

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import "./styles.css";
-import React from 'react';
-import { render } from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-// import Table from 'react-bootstrap/Table';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-import PropTypes from 'prop-types';
-
+import React, { useState } from 'react';
 
 function Capstone() {
   return (
@@ -22,13 +12,38 @@ function Capstone() {
 }
 
 function Header() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
   return (
-    <header>
-      <nav>
-        <ul>
-          <a style={{align: 'right'}} href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
+    <header className="header">
+      <div className="logo">Your Logo</div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about">About</a>
+          </li>
+          <li className="nav-item dropdown">
+            <button onClick={toggleDropdown} className="dropdown-btn">
+              Services
+            </button>
+            {isDropdownOpen && (
+              <div className="dropdown-content">
+                <a href="/service1">Service 1</a>
+                <a href="/service2">Service 2</a>
+                <a href="/service3">Service 3</a>
+              </div>
+            )}
+          </li>
+          <li className="nav-item">
+            <a href="/contact">Contact</a>
+          </li>
         </ul>
       </nav>
     </header>
