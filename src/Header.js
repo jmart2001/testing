1072168b24f48574530fee11d2229ec96f811d81
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-09 09:39:58 -0500

    Styling

--- src/Header.js
+++ src/Header.js
@@ -13,11 +13,11 @@ function Header() {
       <header className="header">
         <div style = {{textAlign: 'left'}} className="logo">WhatToCook</div>
         <nav className="nav">
-          <ul style = {{textAlign: 'right'}} className="nav-list">
-            <a href="/">Home</a>
-            <a href="/RepGen">Recipe Generator</a>
-            <a href="/TriCom">Price Comparator</a>
-            <a href="/Profile">Profile</a>
+          <ul style = {{textAlign: 'right', size: '15px'}} className="nav-list">
+            <a href="/">Home</a> <br />
+            <a href="/RepGen">Recipe Generator</a> <br />
+            <a href="/TriCom">Price Comparator</a> <br />
+            <a href="/Profile">Profile</a> <br />
           </ul>
         </nav>
       </header>
