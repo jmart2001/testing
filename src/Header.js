40530bc1da70980abbe8f3bb877eb3db7b84a478
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-09 09:31:03 -0500

    Styling

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ function Header() {
   
     return (
       <header className="header">
-        <div className="logo">Your Logo</div>
+        <div className="logo" style = {{alight: 'right'}}>WhatToCook</div>
         <nav className="nav">
           <ul className="nav-list">
             <li className="nav-item">
