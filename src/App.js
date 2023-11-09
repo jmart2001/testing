21901a6247e0f90dd232c8c90667b16cb42a9f99
Valerie Duffey <69978259+valerieduffey@users.noreply.github.com>
2023-11-09 09:19:51 -0500

    Imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 import React from 'react';
 import { render } from 'react-dom'
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 // import Table from 'react-bootstrap/Table';
 // import Container from 'react-bootstrap/Container';
